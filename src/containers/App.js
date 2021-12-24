f14f3dbcedfd9aaad0a204f9b4920b8a77af5c0a
josealex7 <alevx197715@gmail.com>
2021-12-24 18:05:23 -0500

    First commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import {Typography} from '@mui/material'
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h3" component="h2">
+        Inicia el proyecto
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
