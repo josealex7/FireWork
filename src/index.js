4d2765f0ab3e5c88d3e3180abf2c802066e19756
Silvia <silviajcn@gmail.com>
2022-02-17 16:10:14 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './containers/App';
+import './styles/style.css';
+import {RegistroApp} from './RegistroApp';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <RegistroApp />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,77 @@
+import React,{useState,useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from '../components/Login';
+import {Registro} from '../components/Registro';
+import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import NavBarUno from '../components/NavBarUno';
+
+export default function AppRouter() {
+
+  const [checking, setChecking] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if(user?.uid){
+         //console.log(user)
+         setIsLoggedIn(true)
+        }
+        else{
+         setIsLoggedIn(false)
+        }
+
+        setChecking(false)
+    })
+
+ 
+}, [setIsLoggedIn,setChecking])
+
+
+if(checking){
+  return(
+      <h1>Espere...</h1>
+  )
+}
+
+
+  return (
+    <Router>
+
+      <NavBarUno />
+      
+        <Routes>
+
+        
+         
+        <Route path="/login" element={
+                    <PublicRoute isAuthenticated={isLoggedIn}>
+                        <Login/> 
+                    </PublicRoute>
+                } />
+
+
+
+                <Route path="/registro" element={
+                    <PublicRoute isAuthenticated={isLoggedIn}>
+                        <Registro/>
+                    </PublicRoute>
+                } />
+
+                
+                <Route path="/*" element={
+                    <PrivateRoute isAuthenticated={isLoggedIn}>
+                        <DashboardRoutes/>
+                    </PrivateRoute>
+                }/>
+        </Routes>
+    </Router>
+  );
+}
